d9e7b63f7f02df6b73a03137ea44498e9ef5bf72
PeixenSen <peix79000@gmail.com>
2025-05-25 16:45:33 +0200

    render-build.sh

--- render-build.sh
+++ render-build.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 
-export PUPPETEER_SKIP_DOWNLOAD=true
-export CHROMIUM_PATH=$(which chromium || which chromium-browser)
+# Installe Chromium
+apt-get update && apt-get install -y chromium
 
+# Définit le chemin de Chrome pour Puppeteer
+export PUPPETEER_EXECUTABLE_PATH=$(which chromium)
+
+# Installe les dépendances
 npm install
