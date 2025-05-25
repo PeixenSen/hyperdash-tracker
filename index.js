a4acb35899bb7c42e8defc22bad23f974b2cd6e2
PeixenSen <peix79000@gmail.com>
2025-05-25 17:05:49 +0200

    index.js

--- index.js
+++ index.js
@@ -1,30 +1,14 @@
 const puppeteer = require('puppeteer');
-const axios = require('axios');
 
 (async () => {
   const browser = await puppeteer.launch({
-    executablePath: process.env.PUPPETEER_EXECUTABLE_PATH,
+    executablePath: process.env.PUPPETEER_EXECUTABLE_PATH || '/usr/bin/chromium',
     headless: true,
     args: ['--no-sandbox', '--disable-setuid-sandbox']
   });
 
   const page = await browser.newPage();
-  await page.goto('https://hyperdash.info/trader/0x225864ad63ba66272cd6be3e65476a2eba48c215', {
-    waitUntil: 'networkidle2',
-    timeout: 60000
-  });
-
-  const html = await page.content();
-
-  const data = {
-    actif: "XAU/USD",
-    direction: "Long",
-    entree: 2438.0,
-    stopLoss: 2432.0,
-    takeProfit: 2455.0,
-    statut: "Ouvert"
-  };
-
+  await page.goto('https://example.com');
+  // ...
   await browser.close();
-  await axios.post('https://hook.eu2.make.com/9hu3u9iddy3gpddnrge86seuhmnwv496', data);
 })();
